91478538556f75b6e66c5c76ae4bfc637b9f1456
richard <richard.noronha24@gmail.com>
2017-09-17 17:27:58 -0600

    Removed the instructors name in the comments

--- example.py
+++ example.py
@@ -25,7 +25,7 @@ __copyright__ = "Copyright (C) 2017 by Richard Noronha"
 # Redistribution, modification or use of this software in source or binary
 # forms is permitted as long as the files maintain this copyright. Users are
 # permitted to modify this and use it to learn about the field of embedded
-# software. Bruce Montgomery, Bhallaji Venkatesankulathusundaram, and the University of Colorado
+# software. Richard Noronha, and the University of Colorado
 # are not liable for any misuse of this material.
 #
 
